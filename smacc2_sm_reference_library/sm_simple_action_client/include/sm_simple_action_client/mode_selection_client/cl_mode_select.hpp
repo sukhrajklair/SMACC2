ee03890f359f3141cdcf109dbbb1f1e21958c952
Sukhraj Klair <sukhraj.k@radskunkworks.com>
2024-06-17 16:52:48 -0400

    added missing license for mode_selection_client and orhtogonals files

--- smacc2_sm_reference_library/sm_simple_action_client/include/sm_simple_action_client/mode_selection_client/cl_mode_select.hpp
+++ smacc2_sm_reference_library/sm_simple_action_client/include/sm_simple_action_client/mode_selection_client/cl_mode_select.hpp
@@ -1,6 +1,27 @@
 #ifndef ROBOT_STATE_MACHINE__CLIENTS__CL_MODE_SELECT_HPP_
 #define ROBOT_STATE_MACHINE__CLIENTS__CL_MODE_SELECT_HPP_
 
+
+// Copyright 2021 RobosoftAI Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*****************************************************************************************************************
+*
+
+Author: Sukhraj Klair
+******************************************************************************************************************/
+
 #include "smacc2/client_bases/smacc_subscriber_client.hpp"
 #include "example_interfaces/msg/int32.hpp"
 
